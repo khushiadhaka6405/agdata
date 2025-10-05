92cd3e4c4bb8872d3afaa388deec8eafa1a92e48
khushiadhaka6405 <khushia6405@gmail.com>
2025-10-05 12:28:15 +0530

    Refactor InMemoryDatabase and remove UserServiceTests
    Updated list initializations in InMemoryDatabase.cs to use
    explicit `new List<Type>()` syntax and corrected the import
    statement for the User entity. Removed UserServiceTests.cs
    along with its using directives and test methods, indicating
    a potential relocation or deprecation of these tests.

--- Infrastructure/Data/InMemoryDatabase.cs
+++ Infrastructure/Data/InMemoryDatabase.cs
@@ -3,21 +3,19 @@ using agdata.Domain.Entities.Product;
 using agdata.Domain.Entities.Redemption;
 using agdata.Domain.Entities.Transaction;
 using agdata.Domain.Entities.user;
-using System;
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace agdata.Infrastructure.Data
 {
     public static class InMemoryDatabase
     {
-        public static List<User> Users { get; set; } = new();
-        public static List<UserAccount> UserAccounts { get; set; } = new();
-        public static List<Product> Products { get; set; } = new();
-        public static List<Event> Events { get; set; } = new();
-        public static List<Redemption> Redemptions { get; set; } = new();
-        public static List<Transaction> Transactions { get; set; } = new();
+        public static List<User> Users { get; set; } = new List<User>();
+        public static List<UserAccount> UserAccounts { get; set; } = new List<UserAccount>();
+        public static List<Product> Products { get; set; } = new List<Product>();
+        public static List<Event> Events { get; set; } = new List<Event>();
+        public static List<Redemption> Redemptions { get; set; } = new List<Redemption>();
+        public static List<Transaction> Transactions { get; set; } = new List<Transaction>();
     }
 }
+
